6be2f8ac315d892de3b3eacc289d72c43349b99c
Sam Smith <hongweiniu0202@gmail.com>
2019-04-02 19:22:07 -0400

    Add existing files

---
+++ .ipynb_checkpoints/sql_queries-checkpoint.py
@@ -0,0 +1,44 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP table songplays"
+user_table_drop = "DROP table users"
+song_table_drop = "DROP table songs"
+artist_table_drop = "DROP table artists"
+time_table_drop = "DROP table time"
+
+# CREATE TABLES
+
+songplay_table_create = "CREATE TABLE IF NOT EXISTS songplays (songplay_id text, start_time int, user_id text, level text, song_id text, artist_id int, session_id int, location text, user_agent text)"
+
+user_table_create = ("CREATE TABLE IF NOT EXISTS users (user_id text, first_name text, last_name text, gender text, level text)")
+
+song_table_create = "CREATE TABLE IF NOT EXISTS songs (song_id text, title text, artist_id text, year int, duration float)"
+
+artist_table_create = ("CREATE TABLE IF NOT EXISTS artists (artist_id text, name text, location text, lattitude text, longitude text)")
+
+time_table_create = ("CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, day int, week int, month int, year int, weekday text)")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("INSERT INTO songplay (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+
+user_table_insert = ("INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)")
+
+song_table_insert = "INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)"
+
+artist_table_insert = ("INSERT INTO artists (artist_id, name, location, lattitude, longitude) VALUES (%s, %s, %s, %s, %s)")
+
+
+time_table_insert = ("INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)")
+
+# FIND SONGS
+
+song_select = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+#drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
+drop_table_queries = [song_table_drop, artist_table_drop]
